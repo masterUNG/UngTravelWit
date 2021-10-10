61ee1d0ad0fed3d192127dce838b6111f6e1fb5d
masterUNG <phrombutr@gmail.com>
2021-10-10 12:27:21 +0700

    update

--- lib/states/authen.dart
+++ lib/states/authen.dart
@@ -18,19 +18,28 @@ class _AuthenState extends State<Authen> {
         return GestureDetector(
           onTap: () => FocusScope.of(context).requestFocus(FocusNode()),
           behavior: HitTestBehavior.opaque,
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Spacer(),
-                buildImage(constraints),
-                buildAppName(),
-                buildUser(constraints),
-                buildPassword(constraints),
-                buildLogin(constraints),
-                const Spacer(),
-                buildCreateAccount(),
-              ],
+          child: Container(
+            decoration: BoxDecoration(color: Colors.purple.withOpacity(0.3)),
+            child: Center(
+              child: CustomScrollView(
+                // mainAxisAlignment: MainAxisAlignment.center,
+                slivers: [
+                  SliverFillRemaining(
+                    child: Column(
+                      children: [
+                        const Spacer(flex: 1,),
+                        buildImage(constraints),
+                        buildAppName(),
+                        buildUser(constraints),
+                        buildPassword(constraints),
+                        buildLogin(constraints),
+                        const Spacer(flex: 2,),
+                        buildCreateAccount(),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         );
@@ -38,19 +47,21 @@ class _AuthenState extends State<Authen> {
     );
   }
 
-  Row buildCreateAccount() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        const ShowText(title: 'Non Account ? '),
-        TextButton(
-            onPressed: () =>
-                Navigator.pushNamed(context, MyConstant.routeCreateAccount),
-            child: ShowText(
-              title: 'Create Account',
-              textStyle: MyConstant().h2Style(),
-            ))
-      ],
+  Container buildCreateAccount() {
+    return Container(margin: const EdgeInsets.only(bottom: 16),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const ShowText(title: 'Non Account ? '),
+          TextButton(
+              onPressed: () =>
+                  Navigator.pushNamed(context, MyConstant.routeCreateAccount),
+              child: ShowText(
+                title: 'Create Account',
+                textStyle: MyConstant().h2Style(),
+              ))
+        ],
+      ),
     );
   }
 
@@ -59,7 +70,7 @@ class _AuthenState extends State<Authen> {
       margin: const EdgeInsets.only(top: 16),
       width: constraints.maxWidth * 0.6,
       child: ElevatedButton(
-        style: ElevatedButton.styleFrom(primary: MyConstant.dart),
+        style: ElevatedButton.styleFrom(primary: MyConstant.dart, shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30),),),
         onPressed: () {},
         child: const Text('Login'),
       ),
@@ -68,13 +79,30 @@ class _AuthenState extends State<Authen> {
 
   Container buildUser(BoxConstraints constraints) {
     return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(30),
+        color: Colors.white.withOpacity(0.5),
+      ),
       margin: const EdgeInsets.only(top: 8),
       width: constraints.maxWidth * 0.6,
+      height: 40,
       child: TextFormField(
-        decoration: const InputDecoration(
+        decoration: InputDecoration(
+          contentPadding: const EdgeInsets.symmetric(vertical: 4),
           hintText: 'User :',
-          prefixIcon: Icon(Icons.perm_identity),
-          border: OutlineInputBorder(),
+          hintStyle: MyConstant().h3Style(),
+          prefixIcon: Icon(
+            Icons.perm_identity,
+            color: MyConstant.dart,
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(30),
+            borderSide: BorderSide(color: MyConstant.dart),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(30),
+            borderSide: BorderSide(color: MyConstant.dart),
+          ),
         ),
       ),
     );
@@ -82,14 +110,31 @@ class _AuthenState extends State<Authen> {
 
   Container buildPassword(BoxConstraints constraints) {
     return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(30),
+        color: Colors.white.withOpacity(0.5),
+      ),
       margin: const EdgeInsets.only(top: 16),
       width: constraints.maxWidth * 0.6,
+      height: 40,
       child: TextFormField(
         obscureText: true,
-        decoration: const InputDecoration(
+        decoration: InputDecoration(
+          contentPadding: const EdgeInsets.symmetric(vertical: 4),
           hintText: 'Password :',
-          prefixIcon: Icon(Icons.lock_outline),
-          border: OutlineInputBorder(),
+          hintStyle: MyConstant().h3Style(),
+          prefixIcon: Icon(
+            Icons.lock_outline,
+            color: MyConstant.dart,
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(30),
+            borderSide: BorderSide(color: MyConstant.dart),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(30),
+            borderSide: BorderSide(color: MyConstant.dart),
+          ),
         ),
       ),
     );

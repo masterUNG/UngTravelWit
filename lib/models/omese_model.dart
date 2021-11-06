eceb8c2ebe1c17ac36fb9f1c15e5a8b8a12b2b3f
masterUNG <phrombutr@gmail.com>
2021-11-06 15:31:04 +0700

    update 6Nov

--- lib/models/omese_model.dart
+++ lib/models/omese_model.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+
 import 'dart:io';
 
 import 'package:dio/adapter.dart';
@@ -26,42 +27,112 @@ class OmeseModel {
   String? status;
 
   Future<String> getToken() async {
-    String publicKey = 'pkey_test_5pjubjexgt8xnb46qbo';
+    String publicKey = 'pkey_test_5pp9n4x6lu8v0riuskf';
     var codec = latin1.fuse(base64);
     var specialKey = codec.encode(publicKey);
 
-    Map<String, dynamic> data = {
-      "expiration_month": '2',
-      "expiration_year": '2022',
-      "name": "Somchai Prasert",
-      "number": "4242424242424242",
-      "security_code": "123",
-      "street1": "476 Fifth Avenue",
-      "city": "New York",
-      "state": "NY",
-      "postal_code": "10320",
-      "country": "US",
-      // "price":"123"
-    };
-
     try {
+      Map<String, dynamic> data = {
+        "expiration_month": '2',
+        "expiration_year": '2022',
+        "name": "Somchai Prasert",
+        "number": "4242424242424242",
+        "security_code": "123",
+      };
+      print('## data ข้อมูลบัตร ==> $data');
+
       OmiseFlutter omiseFlutter = OmiseFlutter(publicKey);
       await omiseFlutter.token
           .create(
-        "Somchai Prasert",
-        "4242424242424242",
-        '2',
-        '2022',
-        "123",
+        data['name'],
+        data['number'],
+        data['expiration_month'],
+        data['expiration_year'],
+        data['security_code'],
       )
-          .then((value) {
-        print('token ==> ${value.id}');
+          .then((value) async {
+        print('## value from omiseFlutter = ${value.toJson()}');
         token = value.id;
+        print('## token = $token');
+
+        String secretKey = 'skey_test_5pj6xiqsml00cgz0ze2';
+        String basicAuth =
+            'Basic ' + base64Encode(utf8.encode(secretKey + ":"));
+        Map<String, String> headerMap = {};
+        headerMap['authorization'] = basicAuth;
+        headerMap['Cache-Control'] = 'no-cache';
+        headerMap['Content-Type'] = 'application/x-www-form-urlencoded';
+
+        Map<String, dynamic> data = {
+          'amount': '200000',
+          'currency': 'thb',
+          'card': token,
+        };
+
+        Uri chargeUri = Uri.parse('https://api.omise.co/charges');
+        http.Response response = await http.post(
+          chargeUri,
+          headers: headerMap,
+          body: data,
+        );
+
+        var resultCharge = json.decode(response.body);
+        print('resultCharge = $resultCharge');
+        print('## status ของการตัดบัตร ==> ${resultCharge['status']}');
       });
+
+      // String urlStr = 'https://vault.omise.co/tokens';
+      //   Uri uri = Uri.parse(urlStr);
+
+      //   String basicAuth =
+      //       'Basic ' + base64Encode(utf8.encode(publicKey + ":"));
+      //   Map<String, String> headerMap = {};
+      //   headerMap['authorization'] = basicAuth;
+      //   headerMap['Cache-Control'] = 'no-cache';
+      //   headerMap['Content-Type'] = 'application/x-www-form-urlencoded';
+
+      // Map<String, String> headerMap = {};
+      // headerMap[HttpHeaders.authorizationHeader] = 'Basic $specialKey';
+
+      // http.Response response = await http.post(
+      //   uri,
+      //   headers: headerMap,
+      //   body: data,
+      // );
+
+      // var result = json.decode(response.body);
+      // var myToken = result['id'];
+
+      // print('result ==> $result');
+      // print('myToken = $myToken');
+
+      // Dio dio = Dio();
+      // (dio.httpClientAdapter as DefaultHttpClientAdapter).onHttpClientCreate =
+      //     (HttpClient client) {
+      //   client.badCertificateCallback =
+      //       (X509Certificate x509certificate, String string, int i) => true;
+      //   return client;
+      // };
+      // dio.options.headers[HttpHeaders.authorizationHeader] = headerMap;
+      // dio.options.headers["Authorization"] = "Basic $specialKey";
+
+      // dio.options.contentType = ContentType("application","x-www-form-urlencoded") as String?;
+      // dio.options.contentType = ContentType(primaryType, subType)
+      // dio.options.headers[HttpHeaders.authorizationHeader] ="Basic $specialKey";
+
+      // Options options = Options(headers: headerMap);
+
+      // await dio
+      //     .post(
+      //       'https://vault.omise.co/tokens',
+      //       data: data,
+      //       options: options,
+      //     )
+      //     .then((value) => print('value ==> $value'));
     } catch (e) {
       print('errer ==> ${e.toString()}');
     }
 
-    return token!;
+    return '';
   }
 }
--- lib/states/omesepayment.dart
+++ lib/states/omesepayment.dart
@@ -4,10 +4,16 @@ import 'package:dio/dio.dart';
 import 'package:file_utils/file_utils.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:omise_flutter/omise_flutter.dart';
 // import 'package:flutter_downloader/flutter_downloader.dart';
 import 'package:ungtravel/models/omese_model.dart';
 import 'package:ungtravel/utility/my_constant.dart';
 import 'package:ungtravel/widgets/show_text.dart';
+
+import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
+import 'dart:convert';
+import 'dart:io';
 
 class OmesePayment extends StatefulWidget {
   const OmesePayment({Key? key}) : super(key: key);
@@ -81,13 +87,101 @@ class _OmesePaymentState extends State<OmesePayment> {
     }
   }
 
+  Future<void> findTokenAnCharge() async {
+    Map<String, dynamic> data = {
+      "expiration_month": 2,
+      "expiration_year": 2024,
+      "name": "Somchai Prasert",
+      "number": "4111111111111111",
+      "security_code": 123,
+      "street1": "476 Fifth Avenue",
+      "city": "New York",
+      "state": "NY",
+      "postal_code": "10320",
+      "country": "US",
+      // "price":"123"
+    };
+
+    var codec = latin1.fuse(base64);
+    var specialKey = codec.encode('pkey_test_5pjubjexgt8xnb46qbo');
+
+    String urlStr = 'https://vault.omise.co/tokens';
+    Uri uri = Uri.parse(urlStr);
+    String basicAuth = 'Basic ' + base64Encode(utf8.encode(publicKey + ":"));
+
+    Map<String, String> headerMap = {};
+    headerMap['authorization'] = basicAuth;
+    headerMap['Cache-Control'] = 'no-cache';
+    headerMap['Content-Type'] = 'application/x-www-form-urlencoded';
+
+    http.Response response = await http.post(
+      uri,
+      headers: headerMap,
+      body: data,
+    );
+
+    print('## res ==> ${response.body.toString()}');
+  }
+
   Widget omeseButton() {
     return ElevatedButton(
-        onPressed: () async {
-          await OmeseModel().getToken().then((value) {
-            print('omise Token ==> $value');
-          });
-        },
-        child: const Text('Req Token'));
+      onPressed: () {
+        // findTokenAnCharge();
+        OmeseModel().getToken();
+      },
+      child: const Text('Req Token'),
+    );
+  }
+
+  String publicKey = 'pkey_test_5pjubjexgt8xnb46qbo';
+
+  Future<void> tokenByPackage() async {
+    OmiseFlutter omiseFlutter = OmiseFlutter(publicKey);
+
+    await omiseFlutter.token
+        .create('Master Ung', '4242424242424242', '2', '2022', '123')
+        .then((value) async {
+      String? token = value.id;
+      print('## token = $token');
+      await omiseFlutter.source
+          .create(
+        1000,
+        'thb',
+        'internet_banking_bay',
+      )
+          .then((value) {
+        print('## value ==>> ${value.toJson()}');
+      });
+    });
+
+    // await tokenByPackage(omiseFlutter);
+
+    // await OmeseModel().getToken().then((value) async {
+    //   String token = value;
+    //   print('omise Token ==> $token');
+
+    //   // await omiseFlutter.capability.retrieve().then((value) {
+    //   //   print('retricve ==> ${value.toJson()}');
+    //   // });
+    // });
+
+    // await omiseFlutter.source
+    //     .create(
+    //   100,
+    //   'thb',
+    //   'internet_banking_bay',
+    //   // terminalId: token,
+    // )
+    //     .then((value) {
+    //   print('source => ${value.toJson()}');
+    // });
+
+    // await OmeseModel().getToken().then((value) async {
+    //   String token = value;
+    //   print('omise Token ==> $token');
+
+    //   // await omiseFlutter.capability.retrieve().then((value) {
+    //   //   print('retricve ==> ${value.toJson()}');
+    //   // });
   }
 }
